3682faba463fe7d36de19c704b9c1d8c2ccac475
Willians Martins de Morais <wsms@gft.com>
2019-05-14 15:33:22 -0300

    add casca inicial do projeto

---
+++ src/test/java/com/williansmartins/manutencaoveiculo/ManutencaoVeiculoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.williansmartins.manutencaoveiculo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManutencaoVeiculoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
