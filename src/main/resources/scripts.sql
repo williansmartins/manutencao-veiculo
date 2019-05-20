cf6f28467940f708b7c1e1e90c4b3147ebda7397
Willians Martins de Morais <wsms@gft.com>
2019-05-20 14:46:50 -0300

    add dao ao projeto

---
+++ src/main/resources/scripts.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `manutencao_veiculo`.`carros` ( `id` INT NOT NULL AUTO_INCREMENT , `fabricante` VARCHAR(255) NOT NULL , `modelo` VARCHAR(255) NOT NULL , `ano` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
